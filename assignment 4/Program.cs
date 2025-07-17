fd28d7b04d316e1e3276581b938e09ed17e4450e
esmaelehab <63879362+esmaelehab@users.noreply.github.com>
2025-07-18 02:21:58 +0300

    question 3

--- assignment 4/Program.cs
+++ assignment 4/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace assignment_4
 {
@@ -20,16 +21,34 @@ namespace assignment_4
             #region q2
             //Write a program that allows the user to insert an integer then print
             //negative if it is negative number otherwise print positive.
-            Console.WriteLine("enter a number :");
-            double number = double.Parse(Console.ReadLine());
-            bool isnegative = number < 0;
-            bool ispositive = number > 0;
-            if (isnegative)
-                Console.WriteLine("negative");
-            else if (ispositive)
-                Console.WriteLine("positive");
-            else
-                Console.WriteLine("not valid");
+            //Console.WriteLine("enter a number :");
+            //double number = double.Parse(Console.ReadLine());
+            //bool isnegative = number < 0;
+            //bool ispositive = number > 0;
+            //if (isnegative)
+            //    Console.WriteLine("negative");
+            //else if (ispositive)
+            //    Console.WriteLine("positive");
+            //else
+            //    Console.WriteLine("not valid");
+
+            #endregion
+            #region q3
+            //Write a program that takes 3 integers from the user then 
+            //prints the max element and the min element.
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine("enter number " + i);
+                int number = int.Parse(Console.ReadLine());
+                if ( number > max)
+                    max = number;
+                if (number < min)
+                    min = number;
+            }
+            Console.WriteLine($"the max is {max}");
+            Console.WriteLine($"the max is {min}");
             #endregion
         }
     }
